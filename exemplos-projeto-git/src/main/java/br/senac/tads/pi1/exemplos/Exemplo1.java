b822939f8f41a31b401406804cb28f783dd19acb
RaiFViana <raifernandosviana@gmail.com>
2021-10-18 21:49:48 -0300

    teste aula

--- exemplos-projeto-git/src/main/java/br/senac/tads/pi1/exemplos/Exemplo1.java
+++ exemplos-projeto-git/src/main/java/br/senac/tads/pi1/exemplos/Exemplo1.java
@@ -16,6 +16,7 @@ public class Exemplo1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world with Git");
+        System.out.println(" Salve salve");
     }
     
 }
